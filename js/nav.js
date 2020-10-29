a342f7b196a18a26b3175742361edac3c9230aa0
Shynqi <shaddamalghafiqih11@gmail.com>
2020-10-29 16:22:53 +0700

    trying fix navbar not appear

--- js/nav.js
+++ js/nav.js
@@ -6,45 +6,56 @@ document.addEventListener('DOMContentLoaded', () => {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
       if (this.readyState === 4) {
-        if (this.status !== 200) {
-          // initialize dropdown
-          const dropdown = document.querySelectorAll('.dropdown-trigger');
-          M.Dropdown.init(dropdown);
+        if (this.status !== 200) return;
 
-          // register event listener for every menu on topnav
-          document.querySelectorAll('.topnav a').forEach((elm) => {
-            elm.addEventListener('click', (event) => {
+        // Muat daftar tautan menu
+        document.querySelectorAll('.topnav').innerHTML = xhr.responseText;
+
+        // initialize dropdown
+        const dropdown = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(dropdown);
+
+        // register event listener for every menu on topnav
+        document.querySelectorAll('.topnav a').forEach((elm) => {
+          elm.addEventListener('click', (event) => {
             // load content
-              page = event.target.getAttribute('href').substr(1);
-              loadPage(page);
-            });
+            page = event.target.getAttribute('href').substr(1);
+            loadPage(page);
           });
-        }
+        });
       }
     };
     xhr.open('GET', '/pages/nav.html', true);
     xhr.send();
   }
+
   // fungsi untuk side navbar
   function sideNav() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
       if (this.readyState === 4) {
-        if (this.status !== 200) {
-          const elems = document.querySelectorAll('.sidenav');
-          M.Sidenav.init(elems);
-          const dropdown = document.querySelectorAll('.dropdown-trigger');
-          M.Dropdown.init(dropdown);
+        if (this.status !== 200) return;
 
-          // register event listener for every menu on topnav
-          document.querySelectorAll('.topnav a').forEach((elm) => {
-            elm.addEventListener('click', (event) => {
-              // load content
-              page = event.target.getAttribute('href').substr(1);
-              loadPage(page);
-            });
+        // Muat daftar tautan menu
+        document.querySelectorAll('.sidenav').innerHTML = xhr.responseText;
+
+        // initialize mataerialize
+        const sidenav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sidenav);
+        const dropdown = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(dropdown);
+
+        // register event listener for every menu on topnav
+        document.querySelectorAll('.topnav a').forEach((elm) => {
+          elm.addEventListener('click', (event) => {
+            // close sidenav
+            M.Sidenav.getInstance(sidenav).close();
+
+            // load content and close sidenav
+            page = event.target.getAttribute('href').substr(1);
+            loadPage(page);
           });
-        }
+        });
       }
     };
     xhr.open('GET', '/pages/nav.html', true);
