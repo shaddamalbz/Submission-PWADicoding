4fcf8e8d4e0bc2bc2163f25cd7df6faa415770d0
Shynqi <shaddamalghafiqih11@gmail.com>
2020-11-02 09:52:27 +0700

    add service worker

--- js/sw.js
+++ js/sw.js
@@ -4,8 +4,23 @@
 /* eslint-disable no-restricted-globals */
 const CACHE_NAME = 'LigaBola_v1';
 const urlsToCache = [
-
+  'index.html',
+  '/pages/home.html',
+  '/pages/nav.html',
+  '/pages/about.html',
+  '/pages/standings.html',
+  '/pages/teams.html',
+  '/img/goal.svg',
+  '/img/Paspoto-square.png',
+  '/css/materialize.min.css',
+  '/css/style.css',
+  '/js/materialize.min.js',
+  '/js/nav.js',
+  '/js/api.js',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
 ];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
